6270dfd5a5ea58e65c445724752abdbf6dc37616
2181148572 <145843384+2181148572@users.noreply.github.com>
2023-11-19 22:00:49 +0300

    lab

--- vehicles/cars/urls.py
+++ vehicles/cars/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.greeting),
-    path('readc/', views.read_cars),
+    path('', views.greeting, name= 'greeting'),
+    path('readc/', views.read_cars, name= 'allcars'),
+    path('readbmw/', views.read_cars, name='bmws'),
+    path('readyear/', views.read_cars, name= 'year__gte=2000'),
+    path('readn/', views.read_cars, name= '1990, 2000'),
+    path('KWT123/', views.read_cars, name= 'license_id'),
+   
 ]
--- vehicles/cars/views.py
+++ vehicles/cars/views.py
@@ -11,3 +11,34 @@ def read_cars(request):
   all_cars = Car.objects.all()
   data['allc'] = all_cars
   return render(request, "read_cars.html", context=data)
+
+def list_bmw_cars(request):
+    data = {}
+    all_cars = Car.objects.filter(Model="BMW")
+    data['allc'] = all_cars
+    return render(request, "read_cars.html", context=data)
+
+def list_2000_cars(request):
+   data = {}
+   all_cars = Car.objects.filter(year__gte=2000)
+   data['allc'] = all_cars
+   return render(request, "read_cars.html", context=data)
+
+def list_1990_2000(request):
+   data = {}
+   all_cars = Car.objects.filter(year_range=["1999", "2000"])
+   data['allc'] = all_cars
+   return render(request, "read_cars.html", context=data)
+   
+def list_cars_mohammad_jamal(request):
+   data = {}
+   person_record = Person.objects.get(name = "mohammad jamal")
+   all_cars = person_record.car_set.all()
+   data['person'] = person_record
+   data['allc'] = all_cars
+   return render(request, "read_cars.html", context=data)
+   
+   
+def search_license_id(request):
+   data = {}
+   return render(request, "read_cars.html", context=data)
